ab8f59f812d69ebf00b855b76c107140ded7ed86
WulfNB <wulfnb@gmail.com>
2019-02-18 22:58:28 +0530

    Added express

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-const http = require('http')
-const fs = require('fs')
+const express = require('express')
 
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-const homePage = fs.readFileSync('home.html')
+const app = express()
 
-// asynchrones process
-// const homePage = fs.readFile('home.html',(file) => {
-
-// })
-
-const server = http.createServer((request,response) =>{
-    console.log(request.url)
-    if (request.url === '/'){
-        response.end(homePage)
-    }
-    else if (request.url === '/about') {
-        response.end(aboutPage)
-    }
-    else if (request.url === '/contact') {
-        response.end(contactPage)
-    }
-    else if (request.url === '/test') {
-        response.end('Test page')
-    }
-    else{
-        response.writeHead(404)
-        response.end('404 . Page not found')
-    }
+app.get('/',(request, response) => {
+    response.json({"name":"Noufal"})
+})
+app.get('/test', (request, response) => {
+    response.send({ "type": "Send test" })
 })
 
-server.listen(3000)
+app.listen(3000)
+
+
+// const http = require('http')
+// const fs = require('fs')
+
+// const aboutPage = fs.readFileSync('about.html')
+// const contactPage = fs.readFileSync('contact.html')
+// const homePage = fs.readFileSync('home.html')
+
+// // asynchrones process
+// // const homePage = fs.readFile('home.html',(file) => {
+
+// // })
+
+// const server = http.createServer((request,response) =>{
+//     console.log(request.url)
+//     if (request.url === '/'){
+//         response.end(homePage)
+//     }
+//     else if (request.url === '/about') {
+//         response.end(aboutPage)
+//     }
+//     else if (request.url === '/contact') {
+//         response.end(contactPage)
+//     }
+//     else if (request.url === '/test') {
+//         response.end('Test page')
+//     }
+//     else{
+//         response.writeHead(404)
+//         response.end('404 . Page not found')
+//     }
+// })
+
+// server.listen(3000)
