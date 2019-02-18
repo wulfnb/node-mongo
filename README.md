6fdca0cae7c78272789d29e256430524743ddf65
WulfNB <wulfnb@gmail.com>
2019-02-18 22:13:02 +0530

    Added more routes

---
+++ README.md
@@ -0,0 +1,3 @@
+# Initial learning of node js
+
+This project is initial learning of node
--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const http = require('http')
 
 const server = http.createServer((request,response) =>{
     console.log(request.url)
-    response.end('Hello sever')
+    if (request.url === '/'){
+        response.end('Hello, Welcome to homepage')
+    }
+    else if (request.url === '/about') {
+        response.end('This is about page')
+    }
+    else if (request.url === '/contact') {
+        response.end('Contact page')
+    }
+    else{
+        response.writeHead(404)
+        response.end('404 . Page not found')
+    }
 })
 
 server.listen(3000)
