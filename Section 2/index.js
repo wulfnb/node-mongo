137f5b2d9be29b624c99965b0490765cadc2e02b
NoUfAL BAbU <noufal@scientiaindia.com>
2019-03-07 14:43:03 +0530

    Completed 16. Added all pages to template engine

--- Section 2/index.js
+++ Section 2/index.js
@@ -16,13 +16,13 @@ app.get('/',(req, res) =>{
     res.render('index')
 })
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
 })
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
 })
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
 })
 
 // app starting
