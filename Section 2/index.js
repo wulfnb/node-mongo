5fbda355beda2da9d3bec098768c0155021ef7ab
NoUfAL BAbU <noufal@scientiaindia.com>
2019-03-07 14:15:31 +0530

    Completed. Added templating engine

--- Section 2/index.js
+++ Section 2/index.js
@@ -1,14 +1,31 @@
-const express = require('express')
 const path = require('path')
+const expressEdge = require('express-edge')
+const express = require('express')
 
 const app = express()
 
+// static files directory
 app.use(express.static('public'))
+// set edge template engine
+app.use(expressEdge)
+// set view for edge
+app.set('views', `${__dirname}/views`)
 
 app.get('/',(req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
+})
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
 })
 
+// app starting
 app.listen(3000,()=>{
     console.log('app started')
 })
